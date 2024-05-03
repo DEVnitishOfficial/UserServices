51cf766082b255fd2c2b7dd7df58683e193516c4
DEVnitishOfficial <nitish.naroun123@gmail.com>
2024-05-03 13:35:44 +0530

    updated middleware

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,27 +1,26 @@
 import multer from "multer";
 import multerS3 from "multer-s3";
-import aws from "aws-sdk";
 import path from "path";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
-import AWS from 'aws-sdk';
-
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+const s3 = new S3Client({
   region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 });
-
-
-const s3 = new aws.S3();
 
 const upload = multer({
   limits: { fileSize: 50 * 1024 * 1024 }, // equivalent to 50 MB
   storage: multerS3({
     s3: s3,
-    bucket:  "cyclic-relieved-plum-moose-ap-southeast-1",
+    bucket: "cyclic-relieved-plum-moose-ap-southeast-1",
+    contentType: multerS3.AUTO_CONTENT_TYPE, // Automatically set the content type
     key: function (req, file, cb) {
       cb(null, Date.now().toString() + path.extname(file.originalname));
-    }
+    },
+    acl: "public-read", // Set ACL (Access Control List) for uploaded files
   }),
   fileFilter: (req, file, cb) => {
     let ext = path.extname(file.originalname);
